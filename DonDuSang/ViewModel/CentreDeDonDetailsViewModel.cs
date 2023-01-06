4e9849f22dac5174a644d48137baaab9b3f2ea12
ThibautFiquet <thibautfiquet792@gmail.com>
2023-01-06 12:00:23 +0100

    Mise en place d'une liste de centre dynamique

--- DonDuSang/ViewModel/CentreDeDonDetailsViewModel.cs
+++ DonDuSang/ViewModel/CentreDeDonDetailsViewModel.cs
@@ -1,5 +1,34 @@
-﻿namespace DonDuSang.ViewModel;
+﻿using DonDuSang.Services;
+using System.Collections.ObjectModel;
+
+namespace DonDuSang.ViewModel;
 
 public partial class CentreDeDonDetailsViewModel : BaseViewModel
 {
+    CentreDeDonService service;
+    ObservableCollection<CentreDeDon> Centres { get; } = new();
+    public CentreDeDonDetailsViewModel(CentreDeDonService service)
+    {
+        Titre = "Don du sang";
+        this.service = service ;
+        GetCentresDeDonCommand = new Command(async () => await GetCentresDeDon());
+    }
+
+    public Command GetCentresDeDonCommand { get; }
+
+    async Task GetCentresDeDon()
+    {
+        if (EstOccupé)
+            return;
+
+        EstOccupé= true;
+        var centres = await service.GetCentresDeDon();
+        if(Centres.Count !=0)
+           Centres.Clear();
+        foreach (var centre in centres)
+        {
+            Centres.Add(centre);
+        }
+        EstOccupé = false;
+    }
 }
